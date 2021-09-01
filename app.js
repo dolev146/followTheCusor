beedca6e1cf6bb9566150b6dfa1de060847dd65a
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-09-01 13:29:18 +0300

    first commit , without the change in size of the cursor

---
+++ app.js
@@ -0,0 +1,7 @@
+var cursor = document.getElementById("cursor");
+document.addEventListener("mousemove", function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+});
